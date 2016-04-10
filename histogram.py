3a518674512b579857a2780bc0cde14d237c9e4e
Matthew Kuzyk <matthew.kuzyk@gmail.com>
2016-04-10 11:01:28 -0400

    Added video reading code and matrix SVD
    Added the code to read video and perform SVD on frames. Cannot get video
    reading working on Windows.

--- histogram.py
+++ histogram.py
@@ -6,17 +6,35 @@ import cv2
  
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required = True, help = "Path to the image")
+ap.add_argument("-v", "--video", required = True, help = "Path to the video")
 args = vars(ap.parse_args())
  
-# load the image and show it
-image = cv2.imread(args["image"])
-y = len(image)
-x = len(image[0])
+# load the video and show it
+vidcap = cv2.VideoCapture(args["video"])
 
-for i in range(0, 3):
-	for j in range(0, 3):
-		window = image[i:(y/3)*(i+1)][j:(x/3)*(j+1)]
-		hist = cv2.calcHist([image], [0, 1, 2], 
-			None, [8, 8, 8], [0, 256, 0, 256, 0, 256]).flatten()
-		A.append(hist)
+frames = []
+A = []
+count = 0;
+success = True
+while success:
+    success,frame = vidcap.read()
+    if success:
+        frames.append(frame)
+
+print "Finished converting video!"
+print frames
+
+for frame in frames:
+	y = len(frame)
+	x = len(frame[0])
+	for i in range(0, 3):
+		for j in range(0, 3):
+			window = frame[i:(y/3)*(i+1)][j:(x/3)*(j+1)]
+			hist = cv2.calcHist([frame], [0, 1, 2], 
+				None, [8, 8, 8], [0, 256, 0, 256, 0, 256]).flatten()
+			A.append(hist)
+
+A = np.array(A)
+A.transpose()
+svd = np.linalg.svd(A)
+print svd
