948714d295cc49a67961ab3023aeae2bf28ffde1
Matthew Kuzyk <matthew.kuzyk@gmail.com>
2016-04-10 12:00:07 -0400

    Cluster metric works
    Got the cluster metric working. Got a hack for Windows to import frames.

--- histogram.py
+++ histogram.py
@@ -3,26 +3,33 @@ from matplotlib import pyplot as plt
 import numpy as np
 import argparse
 import cv2
+import os
  
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", required = True, help = "Path to the video")
+ap.add_argument("-v", "--video", required = False, help = "Path to the video")
+ap.add_argument("-f", "--frames", required = False, help = "Path to the video")
 args = vars(ap.parse_args())
- 
-# load the video and show it
-vidcap = cv2.VideoCapture(args["video"])
-
 frames = []
-A = []
-count = 0;
-success = True
-while success:
-    success,frame = vidcap.read()
-    if success:
-        frames.append(frame)
+A = [] 
+# load the video and show it
+
+if args["video"]:
+	vidcap = cv2.VideoCapture(args["video"])
+	count = 0;
+	success = True
+	while success:
+	    success,frame = vidcap.read()
+	    print "success"
+	    if success:
+	        frames.append(frame)
+
+elif args["frames"]: # Hack for windows
+	for i in os.listdir(args["frames"]):
+		frames.append(cv2.imread(args["frames"] + "\\" + i))
+
 
 print "Finished converting video!"
-print frames
 
 for frame in frames:
 	y = len(frame)
@@ -36,5 +43,15 @@ for frame in frames:
 
 A = np.array(A)
 A.transpose()
-svd = np.linalg.svd(A)
-print svd
+rank = np.linalg.matrix_rank(A)
+svd = np.linalg.svd(A,compute_uv=True)
+V = svd[2]
+norms = []
+for eigenvec in V:
+	norm = 0
+	for j in range(0,rank):
+		norm += np.power(eigenvec[j],2)
+	norms.append(norm)
+
+static_cluster = np.sum(norms)
+print static_cluster
